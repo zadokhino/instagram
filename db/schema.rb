4438071b8d6c6fa441bc08a13294fe7cb3c3d59c
Oleg Zadokhin <zadokhino@gmail.com>
2016-08-31 18:01:26 +0600

    Added model Photo and linked with User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160829183022) do
+ActiveRecord::Schema.define(version: 20160831115842) do
+
+  create_table "photos", force: :cascade do |t|
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+  end
+
+  add_index "photos", ["user_id"], name: "index_photos_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
